e699318004262185076d0aea827de556a9457e59
Suryanshu Raj <2019suryanshu.raj@ves.ac.in>
2022-02-26 13:38:36 +0530

    Initial commit

---
+++ android/app/src/main/java/com/suryanshu/sdkflutter/MainActivity.java
@@ -0,0 +1,7 @@
+package com.suryanshu.sdkflutter;
+
+import io.flutter.embedding.android.FlutterActivity;
+import com.clevertap.android.sdk.ActivityLifecycleCallback;
+import com.clevertap.android.sdk.CleverTapAPI;
+public class MainActivity extends FlutterActivity {
+}
